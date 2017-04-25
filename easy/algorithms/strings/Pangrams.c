813391925753689ed6f05d674ce43055ceed2cb4
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-04-25 21:01:53 +0200

    gets -> scanf

--- easy/algorithms/strings/Pangrams.c
+++ easy/algorithms/strings/Pangrams.c
@@ -7,22 +7,22 @@
 
 int main()
 {
-	int alphabet_indexes[33] = {0}; // 0-25: alphanumeric; 32: space
+	int alphabetIndexes[33] = {0}; // 0-25: alphanumeric; 32: space
 	int i = 0;
 	int isNot = 0;
 
 	char *str = (char *)malloc( 1000 * sizeof( char ) );
 	char s;
 
-	gets( str );
+	scanf( "%999[^\n]s", str );
 
 	while( ( s = str[ i++ ] ) != '\0' )
-		alphabet_indexes[ ( int )s % 97 % 65 ]++; // a-z: 97-122; A-Z: 65-90
+		alphabetIndexes[ ( int )s % 97 % 65 ]++; // a-z: 97-122; A-Z: 65-90
 
 	i = -1;
 
 	while ( ++i < 26 )
-		if ( alphabet_indexes[ i ] == 0 )
+		if ( alphabetIndexes[ i ] == 0 )
 		{
 			isNot = 1;
 			break;
