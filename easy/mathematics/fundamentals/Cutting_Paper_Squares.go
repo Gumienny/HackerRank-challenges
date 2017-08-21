595558a48a5d5dcd8fb2e64edda9a2c9433654b2
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-08-21 15:43:53 +0200

    Create Cutting_Paper_Squares.go

---
+++ easy/mathematics/fundamentals/Cutting_Paper_Squares.go
@@ -0,0 +1,14 @@
+/**
+ * 21/08/2017
+ * https://www.hackerrank.com/challenges/p1-paper-cutting
+ */
+ 
+package main
+import "fmt"
+
+func main() {
+	var n, m int
+    
+	fmt.Scan( &n, &m )
+	fmt.Println( n * m - 1 )
+}
